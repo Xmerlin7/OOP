ac1063660b2baf8c8dc6fa156c25a59d6a471c2c
Seif Allah <saifeldarageely@gmail.com>
2024-03-09 22:05:09 +0200

    #

--- 0x01-firstClass/main/src/App.java
+++ 0x01-firstClass/main/src/App.java
@@ -3,13 +3,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
+        String hola;
         car carObj = new car();
         carObj.carName = "Tesla";
         carObj.maxSpeed = 180;
         car carObj2 = new car();
         carObj2.carName = "Ford";
+        // hola = getCarName();
+        // System.out.println(hola);
         System.out.println(carObj2.carName);
         System.out.println(carObj.carName);
-
+        carObj.setCarName("Kia");
+        System.out.println(carObj.carName);
+        carObj2.setCarName("lada");
+        System.out.println(carObj2.carName);
+        
     }
 }
--- 0x01-firstClass/main/src/car.java
+++ 0x01-firstClass/main/src/car.java
@@ -8,7 +8,17 @@ public class car {
     public void setCarName(String carName) {
         this.carName = carName;
     }
+     
+    //? Getter function */
     public String getCarName() {
         return carName;
+    }
+
+    void setModel(int m){
+        if (m >= 2015)
+            model = m;
+        else
+            System.out.println("sory, we don't accept that model");
+            
     }
 }
