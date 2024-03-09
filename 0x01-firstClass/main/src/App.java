1faabfc2e5374d3b0411be2bae732fb802e028a1
Seif Allah <saifeldarageely@gmail.com>
2024-03-09 20:01:42 +0200

    #

--- 0x01-firstClass/main/src/App.java
+++ 0x01-firstClass/main/src/App.java
@@ -5,7 +5,10 @@ public class App {
         // System.out.println("Hello, World!");
         car carObj = new car();
         carObj.carName = "Tesla";
-        car
+        carObj.maxSpeed = 180;
+        car carObj2 = new car();
+        carObj2.carName = "Ford";
+        System.out.println(carObj2.carName);
         System.out.println(carObj.carName);
 
     }
