1d76e37f23f1fa0b51abd1b323df5fe324e1bbcb
Seif Allah <saifeldarageely@gmail.com>
2024-03-09 22:31:28 +0200

    #

--- 0x01-firstClass/0x02-cirrcle/circle/src/Circle.java
+++ 0x01-firstClass/0x02-cirrcle/circle/src/Circle.java
@@ -1,5 +1,19 @@
 public class Circle {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private double radius;
+    private String color;
+
+    public void setRadius(double r){
+        radius = r;
+    }
+
+    public void setColor(String cl){
+        color = cl;
+    }
+    public double getRadius(){
+        return radius;
+    }
+
+    public String getColor(){
+        return color;
     }
 }
