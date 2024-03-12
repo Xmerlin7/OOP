4b352941ed4491b5f27c3b244c0d09f8ddb66e82
Seif Allah <saifeldarageely@gmail.com>
2024-03-13 00:00:30 +0200

    #

--- 0x03-Constructor/Product/src/App.java
+++ 0x03-Constructor/Product/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //? If i filled those parameters i will use parameterized constructor
+        //? and it will not call the other constructor as it only call one for each object
         Product p1 = new Product();
         p1.dispalyProduct();
     }
