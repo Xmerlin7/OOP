87e6d9bea4a86e4ba641c6e7eb22f584f75c8b1b
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 19:20:26 +0200

    #

--- 0x02-Account/Account/src/Acc.java
+++ 0x02-Account/Account/src/Acc.java
@@ -1,6 +1,12 @@
 public class Acc {
     private int accountNo;
     private String name;
-    private float amount; 
+    private float amount;
+
+    public void insert(int accountNo, String name, float amount) {
+        this.accountNo = accountNo;
+        this.name = name;
+        this.amount = amount;
+    }
+    public void deposit ()
 }
-public 
