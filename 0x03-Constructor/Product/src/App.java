16d3d478d7c4d5796577885896f8b65e580c6dd4
Seif Allah <saifeldarageely@gmail.com>
2024-03-13 18:51:53 +0200

    #

--- 0x03-Constructor/Product/src/App.java
+++ 0x03-Constructor/Product/src/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // If i filled those parameters i will use parameterized constructor
-        // and it will not call the other constructor as it only calls one for each object
-        Product p1 = new Product();
-        
         Test obj = new Test();
         obj.number = 2;
         fun(obj);
