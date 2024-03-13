4094717d91fc9f57ab3fc9d136826cd3e9ff39d0
Seif Allah <saifeldarageely@gmail.com>
2024-03-13 06:44:57 +0200

    #

--- 0x03-Constructor/Product/src/Product.java
+++ 0x03-Constructor/Product/src/Product.java
@@ -6,7 +6,7 @@ public class Product {
     private float discount;
     static int serialNumber = 0;
 
-    //! build a no-Argument constructor, its name same as the class */
+    // ! build a no-Argument constructor, its name same as the class */
     public Product() {
         this.name = "no name";
         this.description = " No description";
@@ -15,9 +15,10 @@ public class Product {
         this.discount = 0;
         serialNumber++;
     }
-    //! build a Parameterized constructor */
-    //? Note: In java u can use same methods different signature
-    //* this called overloading */
+
+    // ! build a Parameterized constructor */
+    // ? Note: In java u can use same methods different signature
+    // * this called overloading */
     public Product(String name, String description, float price, int quantity, float discount) {
         this.name = name;
         this.description = description;
@@ -25,13 +26,20 @@ public class Product {
         this.quantity = quantity;
         this.discount = discount;
     }
+
     public void dispalyProduct() {
-        System.out.println("name "+ this.name);
-        System.out.println("description "+ this.description);
-        System.out.println("price "+ this.price);
-        System.out.println("quantity "+ this.quantity);
-        System.out.println("discount "+ this.discount);
-        System.out.println("SerialNumber is  "+ serialNumber);
+        System.out.println("name " + this.name);
+        System.out.println("description " + this.description);
+        System.out.println("price " + this.price);
+        System.out.println("quantity " + this.quantity);
+        System.out.println("discount " + this.discount);
+        System.out.println("SerialNumber is  " + serialNumber);
     }
 
+    public boolean login(int serialNumberr, String name) {
+    if ((serialNumber == serialNumberr) && this.name.equals(name)){
+        System.out.println("True");
+    }else {
+        System.out.println("false");
+    }
 }
