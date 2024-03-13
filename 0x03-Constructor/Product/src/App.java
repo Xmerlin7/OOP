998f8f9d130c8c9f81477b424b7d2a0076d0c995
Seif Allah <saifeldarageely@gmail.com>
2024-03-13 18:43:44 +0200

    #

--- 0x03-Constructor/Product/src/App.java
+++ 0x03-Constructor/Product/src/App.java
@@ -3,9 +3,12 @@ public class App {
         // If i filled those parameters i will use parameterized constructor
         // and it will not call the other constructor as it only calls one for each object
         Product p1 = new Product();
+        Test obj = new Test();
+        fun(obj);
     }
 
     static void fun(Test x) {
         x.number = 5;
+        System.out.println(x.number);
     }
 }
