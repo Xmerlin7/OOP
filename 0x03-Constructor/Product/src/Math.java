510335fda5908c0f12496b578132d6e087eda932
Seif Allah <saifeldarageely@gmail.com>
2024-03-13 23:11:08 +0200

    #

--- 0x03-Constructor/Product/src/Math.java
+++ 0x03-Constructor/Product/src/Math.java
@@ -1,3 +1,8 @@
 public class Math {
-    
+    int val;
+    public int add(Math obj1, Math obj2){
+        Math obj = new Math();
+        obj.val = obj1.val + obj2.val;
+        return (obj.val);
+    }
 }
