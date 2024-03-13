e75b44bdb4661a63781a9643d8e4f47ea8616d55
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 00:17:30 +0200

    #

--- 0x03-Constructor/Product/src/App.java
+++ 0x03-Constructor/Product/src/App.java
@@ -4,9 +4,10 @@ public class App {
         Math objMath2 = new Math();
         Math result = new Math();
         objMath1.val = 5;
-        objMath2.val = 5;
+        objMath2.val = 7;
         result = objMath1.add(objMath2);
         System.out.println(result.val);
+        System.out.println(objMath1.isEqual(objMath2));
     }
 
 }
--- 0x03-Constructor/Product/src/Math.java
+++ 0x03-Constructor/Product/src/Math.java
@@ -5,4 +5,10 @@ public class Math {
         obj.val = this.val + obj2.val;
         return (obj);
     }
+    boolean isEqual(Math obg2){
+        if (this.val == obg2.val)
+            return true;
+        else
+            return false;
+    }
 }
