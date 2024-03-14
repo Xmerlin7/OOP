8d9ef2baae636d2757e083cbc891d9b9149e5311
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 21:03:27 +0200

    Adding Employee Methods

--- 0x05-Polymorphism/Polymorphism/src/Employee.java
+++ 0x05-Polymorphism/Polymorphism/src/Employee.java
@@ -1,4 +1,28 @@
 public class Employee {
     protected String Name, address;
     protected float salary;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
 }
