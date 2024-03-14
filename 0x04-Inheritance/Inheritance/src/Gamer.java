704b610a08471b205be9cb28cb1a8d93e128b709
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 08:31:50 +0200

    #

--- 0x04-Inheritance/Inheritance/src/Gamer.java
+++ 0x04-Inheritance/Inheritance/src/Gamer.java
@@ -1,15 +1,17 @@
-public class Gamer extends Games{
+public class Gamer extends Games {
     private String player;
     private String playerType;
-    Gamer(){
+
+    Gamer() {
 
     }
-    Gamer (String gameName, int gameYearRelease, int gamePlayTime,
-    String gameCategory, boolean GOTY, String player, String playerType){
+
+    Gamer(String gameName, int gameYearRelease, int gamePlayTime,
+            String gameCategory, boolean GOTY, String player, String playerType) {
         super(gameName, gameYearRelease, gamePlayTime, gameCategory, GOTY);
         this.player = player;
         this.playerType = playerType;
         System.out.println("child child constructor");
     }
-    
+
 }
