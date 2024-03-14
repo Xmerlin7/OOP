6e426b91e46aa7af9ecab8d173225ac3e9c81b71
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 21:22:39 +0200

    #

--- 0x05-Polymorphism/Polymorphism/src/HourlyEmplyee.java
+++ 0x05-Polymorphism/Polymorphism/src/HourlyEmplyee.java
@@ -1,3 +1,13 @@
 public class HourlyEmplyee extends Employee{
+
+    public HourlyEmplyee(String Name, String address, int salary) {
+        super(Name, address, salary);
+        
+    }
+
+    @Override
+    public String toString() {
+        return "HourlyEmplyee []";
+    }
     
 }
--- 0x05-Polymorphism/Polymorphism/src/salaryEmployee.java
+++ 0x05-Polymorphism/Polymorphism/src/salaryEmployee.java
@@ -1,6 +1,15 @@
+
 public class salaryEmployee extends Employee{
     float bonous;
-    public float getBonous() {
-        return bonous;
+    public salaryEmployee(float bonous,String Name, String address, int salary) {
+        super(Name, address, salary);
+        this.bonous = bonous;
     }
+    @Override
+    public float getSalary() {
+        return (super.getSalary() + this.bonous);
+    }
+    
+
+    
 }
