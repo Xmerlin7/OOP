a86d405326acb3b195745333976c469026e8ae88
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 21:13:50 +0200

    #

--- 0x05-Polymorphism/Polymorphism/src/DalilyEmployee.java
+++ 0x05-Polymorphism/Polymorphism/src/DalilyEmployee.java
@@ -1,8 +1,8 @@
 public class DalilyEmployee extends Employee {
-    private float workDailyPrice;
+    private float workDailyPrice, bonous;
     private int workDailyRate;
 
-    public DalilyEmployee(String Name, String address, int salary, float workDailyPrice, int workDailyRate) {
+    public DalilyEmployee(float bonous, String Name, String address, int salary, float workDailyPrice, int workDailyRate) {
         super(Name, address, salary);
         this.workDailyPrice = workDailyPrice;
         this.workDailyRate = workDailyRate;
@@ -14,6 +14,10 @@ public class DalilyEmployee extends Employee {
 
     public void setWorkDailyRate(int workDailyRate) {
         this.workDailyRate = workDailyRate;
+    }
+    @Override
+    public float getSalary() {
+        return (super.getSalary() + this.bonous);
     }
 
 }
