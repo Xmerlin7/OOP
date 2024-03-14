c9cd2de9e332c6964da92dbaa6ecdb13e0c654b2
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 07:21:50 +0200

    #

--- 0x04-Inheritance/Inheritance/src/Games.java
+++ 0x04-Inheritance/Inheritance/src/Games.java
@@ -3,8 +3,36 @@ public class Games {
     protected int gameTime;
     protected String gameCategory;
     protected boolean GOTY;
-    
-    Games (){
+
+    public void setGameTime(int gameTime) {
+        this.gameTime = gameTime;
+    }
+
+    public void setGOTY(boolean gOTY) {
+        GOTY = gOTY;
+    }
+
+    public void setGameCategory(String gameCategory) {
+        this.gameCategory = gameCategory;
+    }
+
+    public void setGameYearPublish(int gameYearPublish) {
+        this.gameYearPublish = gameYearPublish;
+    }
+
+    public String getGameCategory() {
+        return gameCategory;
+    }
+
+    public int getGameTime() {
+        return gameTime;
+    }
+
+    public int getGameYearPublish() {
+        return gameYearPublish;
+    }
+
+    Games() {
         System.out.println("Super constructor");
 
     }
