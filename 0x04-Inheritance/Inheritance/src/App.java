2ca23affbf8f01396c9769e154c88849acffb196
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 07:30:45 +0200

    #

--- 0x04-Inheritance/Inheritance/src/App.java
+++ 0x04-Inheritance/Inheritance/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Gamer gamer = new Gamer();
         gamer.gameName = "The witcher 3";
-        get
+        System.out.println(gamer.getGameName());
 
     }
 }
