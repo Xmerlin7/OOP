79a335eb499f7ed55c7edb3b62d76204969fc088
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 21:04:51 +0200

    #

---
+++ 0x05-Polymorphism/Polymorphism/src/DalilyEmployee.java
@@ -0,0 +1,12 @@
+public class DalilyEmployee {
+    private float workDailyPrice;
+    private int workDailyRate;
+
+    public void setWorkDailyPrice(float workDailyPrice) {
+        this.workDailyPrice = workDailyPrice;
+    }
+
+    public void setWorkDailyRate(int workDailyRate) {
+        this.workDailyRate = workDailyRate;
+    }
+}
