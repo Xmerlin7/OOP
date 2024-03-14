fe448c021869d28de991c7330286e26c79b5bc59
Seif Allah <saifeldarageely@gmail.com>
2024-03-14 21:32:20 +0200

    #

--- 0x05-Polymorphism/Polymorphism/src/App.java
+++ 0x05-Polymorphism/Polymorphism/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Employee e1 = new Employee("string", "null", 5850);
+        final salaryEmployee e2 = new salaryEmployee(50, "null", "null", 2000);
+        System.out.println(e1.getSalary());
+        System.out.println(e2.getSalary());
     }
 }
